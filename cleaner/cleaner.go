77e38d1257041fb054ce808e74ffb041aa481b28
minh <Lminhphu0304@gmail.com>
2021-12-05 13:10:35 +1300

    Move out of main package and create command line parser

---
+++ cleaner/cleaner.go
@@ -0,0 +1,98 @@
+package cleaner
+
+import (
+	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"code.cloudfoundry.org/bytefmt"
+)
+
+type Cleaner struct {
+	dryRun    bool
+	path      string
+	sizeCount int64
+}
+
+var junkFiles = []string{
+	".md",
+	"LICENSE",
+	"license",
+	".txt",
+	".test.",
+	".spec.",
+	"package.json",
+	"Jenkins",
+	".babelrc",
+	".npmrc",
+	".eslintrc",
+	".eslintignore",
+	"Dockerfile",
+	".travis.yml",
+	".zuul.yml",
+	".gitmodules",
+	".npmignore",
+	"Makefile",
+	"makefile",
+	".nycrc",
+	".markdown",
+	".jshintrc",
+	".codeclimate.yml",
+	"editorconfig",
+	".github/",
+	".coveralls.yml",
+	".nyc_output",
+	".circleci",
+	".vscode",
+}
+
+func NewCleaner(dryRun bool, path string) Cleaner {
+	return Cleaner{
+		dryRun: dryRun,
+		path:   path,
+	}
+}
+
+func (c *Cleaner) StartClean() {
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println("Failed to clean node_modules")
+		}
+	}()
+	err := filepath.WalkDir(c.path, c.cleanUp)
+	fmt.Errorf("Error cleaning up node_modules: ", err)
+	fmt.Printf("Total delete size: %sB \n", bytefmt.ByteSize(uint64(c.sizeCount)))
+}
+
+func (c *Cleaner) cleanUp(path string, d fs.DirEntry, err error) error {
+	if d.IsDir() {
+		return nil
+	}
+	for _, junkFile := range junkFiles {
+		if strings.Contains(path, junkFile) {
+			info, err := d.Info()
+			if err != nil {
+				// there's a race condition somewhere in the walkdir so the file can appear twice
+				// ¯\_(ツ)_/¯
+				return nil
+			}
+			c.sizeCount = c.sizeCount + info.Size()
+			if c.dryRun {
+				fmt.Printf("Clean file %s\n", path)
+			} else {
+				deleteFile(path)
+			}
+		}
+	}
+	// fmt.Printf("%s\n", path)
+	return nil
+}
+
+func deleteFile(path string) {
+	err := os.Remove(path)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
--- main.go
+++ main.go
@@ -1,83 +1,21 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"io/fs"
-	"os"
-	"path/filepath"
-	"strings"
 
-	"code.cloudfoundry.org/bytefmt"
+	"github.com/MinhPhu0304/go-clean-node/cleaner"
 )
 
-var sizeCount int64 = 0
-
 func main() {
-	cleanUpNodeModules("./node_modules")
-}
-
-func cleanUpNodeModules(dirPath string) {
-	defer func() {
-		if err := recover(); err != nil {
-			fmt.Println("Failed to clean node_modules")
-		}
-	}()
-	err := filepath.WalkDir(dirPath, cleanUp)
-	fmt.Errorf("Error cleaning up node_modules: ", err)
-	fmt.Printf("Total delete size: %sB \n", bytefmt.ByteSize(uint64(sizeCount)))
-}
-
-var deleteFiles = []string{
-	".md",
-	"LICENSE",
-	"license",
-	".txt",
-	".test.",
-	".spec.",
-	"package.json",
-	"Jenkins",
-	".babelrc",
-	".npmrc",
-	".eslintrc",
-	".eslintignore",
-	"Dockerfile",
-	".travis.yml",
-	".zuul.yml",
-	".gitmodules",
-	".npmignore",
-	"Makefile",
-	".nycrc",
-	".markdown",
-	".jshintrc",
-	".codeclimate.yml",
-	"editorconfig",
-	".github/",
-	".coveralls.yml",
-}
-
-func cleanUp(path string, d fs.DirEntry, err error) error {
-	if d.IsDir() {
-		return nil
+	dryrun := flag.Bool("dryrun", true, "Actually delete it or not")
+	path := flag.String("path", "", "Path to your node_modules. Eg: ./foo/bar/node_modules")
+	flag.Parse()
+	if *path == "" {
+		fmt.Println("Missing file path please try again")
+		return
 	}
-	for _, junkFile := range deleteFiles {
-		if strings.Contains(path, junkFile) {
-			info, err := d.Info()
-			if err != nil {
-				// there's a race condition somewhere in the walkdir so the file can appear twice
-				// ¯\_(ツ)_/¯
-				return nil
-			}
-			sizeCount = sizeCount + info.Size()
-			deleteFile(path)
-		}
-	}
-	fmt.Printf("%s\n", path)
-	return nil
-}
 
-func deleteFile(path string) {
-	err := os.Remove(path)
-	if err != nil {
-		fmt.Println(err)
-	}
+	cleaner := cleaner.NewCleaner(*dryrun, *path)
+	cleaner.StartClean()
 }
